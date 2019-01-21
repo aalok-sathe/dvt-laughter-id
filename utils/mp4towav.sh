17bbf064582fa2af3144d534d5faf678583e6939
aalok.sathe <aalok.sathe@gmail.com>
2019-01-21 15:07:24 -0500

    bugfixes in the script

--- utils/mp4towav.sh
+++ utils/mp4towav.sh
@@ -1,9 +1,9 @@
 #/bin/bash
 # usage: ./mp4towav.sh BASE_DIRECTORY
 #   where BASE_DIRECTORY is the directory housing mp4 files.
-#   output is generated in the same directory as mp4 files.
+#   output is generated in <directory>/../wav/
 
-for file in "$1".mp4
+for file in "$1"/*.mp4
 do
-	ffmpeg -i "$file" -vn -acodec pcm_s16le -ac 1 "$(basename "$file" .mp4).wav"
+	ffmpeg -i "$file" -vn -acodec pcm_s16le -ac 1 "$1/../wav/$(basename "$file" .mp4).wav"
 done
