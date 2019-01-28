40a7127f2620cab7d13508235f71c8e9a0d44ce3
aalok.sathe <aalok.sathe@gmail.com>
2019-01-28 09:46:33 -0500

    add argument check and usage instr 	modified:   utils/mp4towav.sh

--- utils/mp4towav.sh
+++ utils/mp4towav.sh
@@ -4,6 +4,12 @@
 #   where BASE_DIRECTORY is the directory housing mp4 files.
 #   output is generated in BASE_DIRECTORY/../wav/
 
+if [ $# -eq 0 ]; then
+    echo -e "usage: ./utils/mp4towav.sh BASE_DIRECTORY"
+    echo -e "\tBASE_DIRECTORY: directory housing the mp4 video files"
+    exit
+fi
+
 for file in "$1"/*.mp4
 do
     if [ ! -f "$1/../wav/$(basename "$file" .mp4).wav" ]; then
