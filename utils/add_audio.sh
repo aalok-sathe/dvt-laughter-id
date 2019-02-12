5aec1f046ac953b31de8385078804147bc647a9a
aalok.sathe <aalok.sathe@gmail.com>
2019-02-12 09:01:37 -0500

    add codec arg and now it works (also compresses video, apparently)

--- utils/add_audio.sh
+++ utils/add_audio.sh
@@ -2,4 +2,4 @@
 # this script takes three arguments: vid, aud, out and creates output at out
 #   which is a video file corresponding to vid with aud audio added to it
 
-ffmpeg -i "$1" -i "$2" -codec copy -shortest "$3"
+ffmpeg -i "$1" -i "$2" -c:v libx264 -c:a libvorbis -shortest "$3"
